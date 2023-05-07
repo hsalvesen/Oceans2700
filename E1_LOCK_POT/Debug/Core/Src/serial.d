a3432bf4379a18215d806ff02c9369090f4d111e
Haz <harrysalvesen@gmail.com>
2023-05-07 19:00:44 +1000

    Lock Pot progress

---
+++ E1_LOCK_POT/Debug/Core/Src/serial.d
@@ -0,0 +1,16 @@
+Core/Src/serial.o: ../Core/Src/serial.c ../Core/Inc/serial.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h
+../Core/Inc/serial.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
