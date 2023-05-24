aa3052de1505786f8c0e0ce61fd6fcfabf289c42
sam-richards <77648080+sam-richards@users.noreply.github.com>
2023-05-24 22:32:54 +1000

    Added GUI Code

---
+++ GUI/main.py
@@ -0,0 +1,10 @@
+from UARTcomms import *
+from GUI import GUI
+from gui_state_machine import *
+
+if __name__ == "__main__":
+    # Initialise the GUI State Machine and update it until the window is closed
+    g = GUIStateMachine(GUI(), UARTcomms(debug=True), UARTcomms(debug=True), UARTcomms(), UARTcomms(debug=True))
+    while not g.do_state():
+        pass
+        
